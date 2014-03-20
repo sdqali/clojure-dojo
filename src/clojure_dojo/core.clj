c8b97219de6ec4577cc3e4cdac860658f7039c29
Sadique Ali <sadiqalikm@gmail.com>
2014-03-20 01:12:59 -0700

    Use cond instead of if.

--- src/clojure_dojo/core.clj
+++ src/clojure_dojo/core.clj
@@ -3,13 +3,12 @@
 (defn edit-distance
   "Return the Levenshtein edit distance between two strings"
   [first second]
-  (if (empty? second)
-    (count first)
-    (if (empty? first)
-      (count second)
-      (min (+ 1 (edit-distance (drop 1 first) second))
-           (+ 1 (edit-distance first (drop 1 second)))
-           (+ (if (= (take 1 first) (take 1 second))
-                0
-                1)
-              (edit-distance (drop 1 first) (drop 1 second)))))))
+  (cond
+   (empty? first) (count second)
+   (empty? second) (count first)
+   :else (min (+ 1 (edit-distance (drop 1 first) second))
+              (+ 1 (edit-distance first (drop 1 second)))
+              (+ (cond
+                  (= (take 1 first) (take 1 second)) 0
+                  :else 1)
+                 (edit-distance (drop 1 first) (drop 1 second))))))
